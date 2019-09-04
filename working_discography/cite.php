215959cfbd5054de10276359acd35ac4409285cd
Nick Ferrari <njferrari14@gmail.com>
2019-09-04 08:09:52 -0500

    Changed file extensions from html to php, updated folder structure for clarity, and added .gitignore

---
+++ working_discography/cite.php
@@ -0,0 +1,127 @@
+
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Queen</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="styles.css">
+  <link href="https://fonts.googleapis.com/css?family=Peralta" rel="stylesheet"> 
+ </head>
+
+
+
+<body>
+  <!--navigation-->
+	 <nav class="navbar sticky-top navbar-dark bg-danger">
+    <a class="navbar-brand" href="index.html">Band</a>
+    <a class="navbar-brand" href="music.html">Music</a>
+    <a class="navbar-brand" href="fun.html">Fun Facts</a>
+    <a class="navbar-brand" href="cite.html">Cite Info</a>
+  </nav>
+
+
+
+<!--top image-->
+<div class="container"  style="background: #C9BF67; padding-top:12px; padding-bottom:15px;">
+ <div class="row">
+ <div class="col-12">
+ <div class="card  text-dark">
+ <img class="card-img-top" src="../images/cite.jpg" alt="Card image cap">
+</div>
+  
+  <!--cite info-->
+  <div class="card-body">
+    <h4 class="card-title">Cite Info</h4>
+     <p class="card-text">
+        Album info: <a class="cite-link" href="http://www.icce.rug.nl/~soundscapes/VOLUME03/Queen_anomaly_Appendix1.shtml">Albums</a><br>
+        Funfacts: <a class="cite-link" href="https://ohfact.com/interesting-facts-about-band-queen/">Oh Facts </a><br>
+        Photos:<a class="cite-link" href="https://www.google.com/"> Photos</a><br>
+        Personal Info:<a class="cite-link" href="https://en.wikipedia.org"> Member info</a><br>
+        Quote:<a class="cite-link" href="https://www.brainyquote.com/authors/freddie_mercury"> Brainy quote</a><br>
+        video<a class="cite-link" href="https://www.youtube.com/watch?v=A22oy8dFjqc"> Youtube</a><br>
+      </p>
+    </div>
+  </div>
+ </div>
+</div><br>
+<!--video-->
+<div class="container" style="background: #C9BF67; padding-top:14px; padding-bottom:14px;">
+     <div class="row">
+     <div class="col-12">
+     <div class="embed-responsive embed-responsive-16by9">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/A22oy8dFjqc"  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+  </div>
+</div>
+</div><br>
+
+<!--blockquote-->
+<div class="container">
+        <div class="row">
+        <div class="col-12">
+        <div class="shadow-lg p-3 mb-5 bg rounded">
+        <blockquote class="blockquote blockquote-reverse" style="padding-left:10px; font-size:40px;">
+        <p class="mb-0">We're a bit flashy, but the musics's not one big noise. </p>
+        <footer class="blockquote-footer">Freddie Mercury
+        <cite title="Source Title">brainyquote</cite>
+      </footer>
+     </blockquote>
+    </div>
+   </div>
+  </div>
+</div>
+
+
+
+<!--feedback form-->
+<div class="container" style="background: #C9BF67;padding-top:15px; padding-bottom:15px;">
+      <div class="row">
+      <div class="col-12">		
+      <div class="col bg-danger text-white" style="padding-top: 15px; padding-left:25px; padding-bottom: 10px;">
+      <form>
+      <h4>JOIN THE FAN CLUB!!</h4>
+      <div class="form-group col">
+      <label for="name">Name</label>
+      <input type="text" class="form-control" id="name" placeholder="First and last name">
+  </div>
+<div class="form-group col">
+      <label for="address">Address</label>
+      <input type="text" class="form-control" id="address" placeholder="City, State and Zip">
+</div>
+  
+<div class="form-group col">
+      <label for="birthday">Birthday</label>
+      <input type="text" class="form-control" id="birthday" placeholder="01/18/1978">
+</div>
+
+<div class="form-group col">
+      <label for="email">Email</label>
+      <input type="text" class="form-control" id="email" placeholder="email@example.com">
+</div>
+
+<div class="form-group col">
+      <label for="comments">Comments</label>
+      <input type="text" class="form-control" id="comments" placeholder="Comments">
+     </div>
+    </form>
+   </div>
+  </div>
+ </div>
+</div>
+
+
+
+
+
+
+<script src="js/jquery.slim.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
+
---
+++ working_discography/fun.php
@@ -0,0 +1,130 @@
+
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Queen</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="styles.css">
+  <link href="https://fonts.googleapis.com/css?family=Peralta" rel="stylesheet">
+ </head>
+
+<body>
+<!--navigation-->
+<nav class="navbar sticky-top navbar-dark bg-danger">
+  <a class="navbar-brand" href="index.html">Band</a>
+  <a class="navbar-brand" href="music.html">Music</a>
+  <a class="navbar-brand" href="fun.html">Fun Facts</a>
+  <a class="navbar-brand" href="cite.html">Cite Info</a>
+</nav>
+
+<!--top image funfacts 1-5-->
+<div class="container" style="background: #C9BF67; padding-top:10px; padding-bottom:15px;">
+    <div class="row">
+    <div class="col-12">
+    <div class="card">
+     <img class="card-img-top" src="../images/band.jpg" alt="band_logo" style= "width:100%;z-index:0;">
+    <div class="card-body  text-dark">
+    <h4 class="card-title">Fun Facts</h4>
+    <p class="card-text">
+      
+    1. Not all artists are drop-outs
+    People often think that rock stars dedicate their whole life to music and hence are not much good in studies. A big blow to such misconception is the Queen Band. Except Freddie Mercury, every other band member has a post graduate degree.<br>
+    2. Freddie Mercury aka Farrokh Bulsara
+    When Freddie Mercury was born, he was named Farrokh Bulsara as he belonged to an Indian family from Gujarat. But later when he was studying in a boarding school, he adopted the name Freddie Mercury. Strangely his name in the passport is Fredrick Mercury. <br>
+    3. Freddie named the band “Queen”
+    Freddie knew that naming a male band “Queen” would raise some eyebrows and would be associated with gay terms. Yet he went ahead with this name as he thought it to be a regal, strong and splendid sounding name. <br>
+    4. “I want it all” inspired by Brian’s wife
+    Brian May had claimed that the main inspiration behind the song “I want it all” was his wife Anita Dobson. He got the idea when his wife said I want it all and I want it now! <br>
+    5. The accidental invention of stand-free mic </p>
+      </div>
+     </div>
+    </div>
+   </div>
+</div><br>
+
+<!--image 2 funfacts 6-8-->
+<div class="container"  style="background: #C9BF67; padding-top:15px; padding-bottom:15px;">
+      <div class="row">
+      <div class="col-12">
+      <div class="card">
+       <img class="card-img-top" src="../images/Queen-live.jpg" alt="band_logo" style= "width:100%;">
+      <div class="card-body text-dark">
+      <h4 class="card-title">Fun Facts</h4>
+      <p class="card-text">
+      6. “I like to ride my bicycle” but Freddie didn’t like to ride bicycle!
+      Quite contrary to the lyrics of the song “Bicycle Race”, Freddie Mercury didn’t like riding <br>
+      7. Do you know that the song “Crazy little thing called love” was composed in a bath? And guess what? It was composed by Freddie Mercury himself. Freddie was actually in a hotel and while bathing in a tub he got inspired for this song. He even had the piano brought near his tub so that he can compose the song. <br>
+      8. Two music videos of Queen was filmed in Roger Taylor’s garden
+      The drummer of Queen, Roger Taylor’s garden is as famous as Roger Taylor himself. This is due to the fact that the videos of “Spread your wings” and “I want it all” were recorded in his garden. He really got inspired by his wife I guess!</p>
+     </div>
+    </div>
+   </div>
+  </div>
+</div><br>
+
+<!--image 3 funfacts 9&10-->
+<div class="container"  style="background: #C9BF67; padding-top:15px; padding-bottom:15px;">
+      <div class="row">
+      <div class="col-12">
+      <div class="card">
+       <img class="card-img-top" src="../images/FreddieMJ.jpg" alt="freddiemj" style= "width:100%;">
+      <div class="card-body  text-dark">
+      <h4 class="card-title">Fun Facts</h4>
+      <p class="card-text">
+      9. MJ insisted to release “Another one bites the dust” At first Queen didn’t plan to release the song “Another one bites the dust”. But when Michael Jackson heard the song at the backstage of an L.A. concert, he convinced the band to release it. The song became one of the most successful singles of the band and sold over seven million.<br>
+      10. Queen honored by England: In 1999, England honored the Queen band by releasing a postage stamp commemorating Freddie Mercury. But this stamp created unpleasantness among the royal family as the photo chosen for the stamp had Roger Taylor in background. According to British tradition, the only living people who can appear on their stamps are the member of Royal Family.</p>
+     <img class="card-img-top" src="../images/stamp.jpg" alt="stamp" style= "width:100%;">
+      </div>
+     </div>
+    </div>
+   </div>
+</div><br>
+
+
+<!--feedback form-->
+<div class="container" style="background: #C9BF67;padding-top:15px; padding-bottom:15px;">
+      <div class="row">
+      <div class="col-12">    
+      <div class="col bg-danger text-white" style="padding-top: 15px; padding-left:25px; padding-bottom: 10px;">
+      <form>
+      <h4>JOIN THE FAN CLUB!!</h4>
+      <div class="form-group col">
+      <label for="name">Name</label>
+      <input type="text" class="form-control" id="name" placeholder="First and last name">
+  </div>
+<div class="form-group col">
+      <label for="address">Address</label>
+      <input type="text" class="form-control" id="address" placeholder="City, State and Zip">
+</div>
+  
+<div class="form-group col">
+      <label for="birthday">Birthday</label>
+      <input type="text" class="form-control" id="birthday" placeholder="01/18/1978">
+</div>
+
+<div class="form-group col">
+      <label for="email">Email</label>
+      <input type="text" class="form-control" id="email" placeholder="email@example.com">
+</div>
+
+<div class="form-group col">
+      <label for="comments">Comments</label>
+      <input type="text" class="form-control" id="comments" placeholder="Comments">
+     </div>
+    </form>
+   </div>
+  </div>
+ </div>
+</div>
+
+
+<script src="js/jquery.slim.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
